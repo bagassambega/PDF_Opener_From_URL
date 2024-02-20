2083a9e79bbcfddce76907da59a91b24973e8ce0
Bagas <bagassambega@gmail.com>
2024-02-20 11:38:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pdfurl/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.pdfurl;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+import android.os.Bundle;
+import android.view.View;
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    PDFView pdfView;
+//    String pdfurl = findViewById(R.id.input_url).toString();
+    String pdfurl = "https://faculty.kfupm.edu.sa/ics/alfy/files/teaching/ics431/slides/ch5r_CPU-Scheduling.pdf";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pdfView = findViewById(R.id.idPDFView);
+        new RetrievePDFfromUrl().execute(pdfurl);
+    }
+
+    class RetrievePDFfromUrl extends AsyncTask<String, Void, InputStream> {
+        @Override
+        protected InputStream doInBackground(String... strings) {
+            // we are using inputstream
+            // for getting out PDF.
+            InputStream inputStream = null;
+            try {
+                URL url = new URL(strings[0]);
+                // below is the step where we are
+                // creating our connection.
+                HttpURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+                if (urlConnection.getResponseCode() == 200) {
+                    // response is success.
+                    // we are getting input stream from url
+                    // and storing it in our variable.
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                }
+
+            } catch (IOException e) {
+                // this is the method
+                // to handle errors.
+                e.printStackTrace();
+                return null;
+            }
+            return inputStream;
+        }
+
+        @Override
+        protected void onPostExecute(InputStream inputStream) {
+            // after the execution of our async
+            // task we are loading our pdf in our pdf view.
+            pdfView.fromStream(inputStream).load();
+        }
+    }
+}
